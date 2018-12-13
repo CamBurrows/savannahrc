0af741c08f9cb387dac4b9e8b31c08afcc75b733
Cam Burrows <cameron@speeddigital.com>
2018-12-12 19:23:52 -0500

    all photos fixed and gallery show hide in there

--- savannahrc-new/source/javascripts/site.js
+++ savannahrc-new/source/javascripts/site.js
@@ -2,4 +2,15 @@ $(document).ready(function(){
   $(".down-arrow").on('click', function(){
     $("html,body").animate({ scrollTop: window.pageYOffset + $(window).height()}, 300);
   })
+
+  $(".extend-gallery-button").on('click', function(){
+    if ($("#thumbnails").hasClass('open-gallery')){
+      $("#thumbnails").removeClass('open-gallery');
+      $(".extend-gallery-button").html('More Images');
+    }
+    else {
+      $("#thumbnails").addClass('open-gallery');
+      $(".extend-gallery-button").html('Less Images');
+    }
+  })
 })
